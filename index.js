e8f6e286f2103ee439957b0433a7ab151686db12
devthalal <thalal.hassan@neoito.com>
2023-02-23 18:07:06 +0530

    test

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-
+import { shared } from "@appblocks/node-sdk";
 
 const handler = async (event) => {
+  const sharedData = await shared.getShared();
 
-  const {req, res} = event
-  
+  const { req, res } = event;
+
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    res.write(JSON.stringify({ success: true, msg: "Health check success" }));
+    res.end();
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
-  res.end()
-  
-}
+  const testShFun = await sharedData.testShFun();
 
-export default handler
+  // Add your code here
+  res.write(
+    JSON.stringify({
+      success: true,
+      msg: `Happy Hacking`,
+      testShFun,
+      testShFun2: sharedData.testShFun2(),
+    })
+  );
+  res.end();
+};
+
+export default handler;
